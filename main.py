21bbe2c52b3b2e6cee2b16daf13a6a17ddee2d83
강지원 <kangji149@naver.com>
2023-07-26 10:10:48 +0900

    add feature short url

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import shorten
+
+app = FastAPI()
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/")
+async def read_index():
+    return FileResponse('public/index.html',media_type='text/html')
 
 
+@app.get("/url/short")
+async def shorten_URL(url):
+    short_url = shorten.shorten_url(url)
+    return {"message": "success","url":short_url}
+
+app.mount("/", StaticFiles(directory="public"), name="static")
